52630dffacb6f537771e1981b091187393f1d04d
Soares Chen <soares.chen@gmail.com>
2013-12-16 03:43:38 +0800

    Added noCopy mechanism.

---
+++ test/no-copy.js
@@ -0,0 +1,36 @@
+
+'use strict'
+
+var should = require('should')
+var copyLib = require('../lib/copy.js')
+
+describe('no copy test', function() {
+  it('should copy', function() {
+    var obj = { foo: 'foo' }
+
+    var copy = copyLib.copyObject(obj)
+    copy.foo = 'bar'
+
+    should.equal(obj.foo, 'foo')
+  })
+
+  it('should not copy', function() {
+    var obj = { foo: 'foo' }
+    obj._noCopy = true
+
+    var copy = copyLib.copyObject(obj)
+    copy.foo = 'bar'
+
+    should.equal(obj.foo, 'bar')
+  })
+
+  it('non-enumerable no copy', function() {
+    var obj = { foo: 'foo' }
+    copyLib.noCopy(obj)
+
+    var copy = copyLib.copyObject(obj)
+    copy.foo = 'bar'
+
+    should.equal(obj.foo, 'bar')
+  })
+})

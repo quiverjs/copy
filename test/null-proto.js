f1d01dcc63a57aa705c5829a2185eba8bc8887d0
Soares Chen <soares.chen@gmail.com>
2013-12-19 01:25:47 +0800

    Make copied plain objects have null prototype.

--- test/null-proto.js
+++ test/null-proto.js
@@ -14,4 +14,22 @@ describe('null proto test', function() {
 
     should.equal(obj.foo, 'foo')
   })
+
+  it('copied object should be null proto', function() {
+    var obj = {
+      foo: 'foo'
+    }
+    var originalProto = Object.getPrototypeOf(obj)
+    var defaultProto = Object.getPrototypeOf({ })
+
+    should.equal(originalProto, defaultProto)
+    should.exist(obj.toString)
+
+    var copy = copyObject(obj)
+    var copyProto = Object.getPrototypeOf(copy)
+
+    should.notEqual(originalProto, copyProto)
+    should.equal(copyProto, null)
+    should.not.exist(copy.toString)
+  })
 })
